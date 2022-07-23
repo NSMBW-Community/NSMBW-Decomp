45f67445e476e9454beaa87787a658874c54a0a8
RootCubed <liam@rootcubed.dev>
2022-07-23 02:03:09 +0200

    Remake slice info file in JSON

--- tools/slice_rel.py
+++ tools/slice_rel.py
@@ -2,7 +2,9 @@
 # Slices a REL file into .o files
 
 import argparse
+import json
 from pathlib import Path
+from typing import TypedDict
 from relfile import REL
 
 from elffile import *
@@ -11,6 +13,11 @@ from elfconsts import *
 parser = argparse.ArgumentParser(description='Slices REL files')
 parser.add_argument('rel_files', type=Path, nargs='+')
 args = parser.parse_args()
+
+class JSONSliceData(TypedDict):
+    name: str
+    compilerFlags: str
+    memoryRanges: dict[str, str]
 
 class RelocSym:
     # Models a symbol referenced by a relocation entry
@@ -26,14 +33,15 @@ class RelocSym:
         return self.__repr__()
 
 class SliceSection:
-    def __init__(self, sec_name: str, sec_idx: int, start_offs: int, end_offs: int):
+    def __init__(self, sec_name: str, sec_idx: int, start_offs: int, end_offs: int, alignment):
         self.sec_name = sec_name
         self.sec_idx = sec_idx
         self.start_offs = start_offs
         self.end_offs = end_offs
+        self.alignment = alignment
 
     def __repr__(self) -> str:
-        return f'<SliceSection {self.sec_name} (#{self.sec_idx}) {self.start_offs:0x}-{self.end_offs:0x}>'
+        return f'<SliceSection {self.sec_name} (#{self.sec_idx}) {self.start_offs:0x}-{self.end_offs:0x}, align={self.alignment}>'
 
     def contains(self, reloc_sym: RelocSym):
         return reloc_sym.section == self.sec_idx and self.start_offs <= reloc_sym.addend <= self.end_offs
@@ -68,17 +76,6 @@ def read_reloc_refs(rel_file: REL, idx: int):
                 reloc_syms.add(dest_sym)
                 module_relocs[idx][this_loc] = (dest_sym, reloc.reloc_type)
 
-# TODO: store this information elsewhere
-section_aligns = {
-    '.init': 1,
-    '.text': 0x10,
-    '.ctors': 0x4,
-    '.dtors': 0x4,
-    '.data': 0x8,
-    '.rodata': 0x8,
-    '.bss': 0x4
-}
-
 def extract_slice(rel_file: REL, slice: Slice):
     elf_file = ElfFile(ET.ET_REL, EM.EM_PPC)
 
@@ -97,10 +94,8 @@ def extract_slice(rel_file: REL, slice: Slice):
             sec_data = rel_file.sections[sec.sec_idx].get_data()[sec.start_offs:sec.end_offs]
             elf_sec = ElfSection(sec.sec_name, bytes(sec_data))
             elf_sec.header.sh_type = SHT.SHT_PROGBITS
-        if sec.sec_name in section_aligns:
-            elf_sec.header.sh_addralign = section_aligns[sec.sec_name]
-        else:
-            elf_sec.header.sh_addralign = 0x10
+
+        elf_sec.header.sh_addralign = sec.alignment
         elf_file.add_section(elf_sec)
 
     for sec in slice.slice_secs:
@@ -133,17 +128,6 @@ def extract_slice(rel_file: REL, slice: Slice):
 
 idx = 1
 
-# TODO: store this information elsewhere
-section_idx = {
-    '.init': -1,
-    '.text': 1,
-    '.ctors': 2,
-    '.dtors': 3,
-    '.data': 4,
-    '.rodata': 5,
-    '.bss': 6
-}
-
 rel: Path
 for rel in args.rel_files:
     if not rel.is_file():
@@ -155,19 +139,20 @@ for rel in args.rel_files:
         read_reloc_refs(rel_file, idx)
 
         # Read slices
-        with open(f'slices/{rel.with_suffix(".csv").name}', "r") as slice_file:
-            csv_header = slice_file.readline().rstrip().split(',')
-            section_names = [x.split('$start')[0] for x in csv_header if '$start' in x]
-            for line in slice_file:
-                splitted = line.rstrip().split(',')
-                slice_name = splitted[0]
-                slice_sections = []
-                for i in range(1, len(splitted), 2):
-                    if splitted[i] != '':
-                        name = section_names[i // 2]
-                        begin = int(splitted[i], 16)
-                        end = int(splitted[i + 1], 16)
-                        slice_sections.append(SliceSection(name, section_idx[name], begin, end))
+        with open(f'slices/{rel.with_suffix(".json").name}', 'r') as slice_file:
+            slice_json = json.load(slice_file)
+            slice_meta = slice_json['meta']
+            slice_section_info: dict[str, dict] = slice_meta['sections']
+
+            slice: JSONSliceData
+            for slice in slice_json['slices']:
+                slice_name = slice['name']
+                slice_sections: list[SliceSection] = []
+                for sec_name in slice['memoryRanges']:
+                    range_str = slice['memoryRanges'][sec_name]
+                    begin, end = [int(x, 16) for x in range_str.split('-')]
+                    sec_info = slice_section_info[sec_name]
+                    slice_sections.append(SliceSection(sec_name, sec_info['index'], begin, end, sec_info['align']))
                 slice = Slice(slice_name, slice_sections)
             
                 elf = extract_slice(rel_file, slice)
