0a352b678072d14d2a0807eb75e6e73b33f6830f
CLF78 <twomp021@gmail.com>
2022-09-13 12:34:29 +0200

    Add initial decompilation

---
+++ include/lib/MSL_C/math/fmod.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double fmod(double x, double mod);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ include/lib/nw4r/math/constants.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <types.h>
+
+namespace nw4r {
+namespace math {
+
+const f32 F_ULP = (float)(0x34000000); // Minimum positive value that satisfies 1.0f + x != 1.0f
+
+} // namespace math
+} // namespace nw4r
