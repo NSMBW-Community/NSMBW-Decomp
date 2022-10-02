8c1f4c7c2dca94f521ced1b238ac5e5d452dd19d
RootCubed <liam@rootcubed.dev>
2022-10-02 08:14:51 +0200

    Try to fix Shift-JIS issues

---
+++ include/sjis_constants.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// [This file must be edited in Shift JIS encoding]
+
+// [Translation: Heap that each process can have individually (fBase_c::mHeap)]
+#define F_BASE_HEAP_NAME "各プロセスが個別で持てるヒープ(fBase_c::mHeap)"
