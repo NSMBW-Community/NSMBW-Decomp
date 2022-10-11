03b780458f59c99c2a61b4273e3123a85eac4a4a
CLF78 <twomp021@gmail.com>
2022-10-11 12:00:41 +0200

    Finish refactoring (see description)
    - Finish reorganizing fBase_c
    - Improve some comments in dBase_c
    - Make doxygen detect g_basesRandomSeed
    - Remove unnecessary copydoc commands

--- include/dol/bases/d_base.hpp
+++ include/dol/bases/d_base.hpp
@@ -1,47 +1,44 @@
 #pragma once
 #include <dol/framework/f_base.hpp>
 #include <dol/cLib/c_owner_set.hpp>
+/// @file
 
 /// @brief An extension of fBase_c with base kind and name strings.
 /// @details All bases should inherit from this class, not from fBase_c.
 class dBase_c : public fBase_c, public cOwnerSetMg_c {
     public:
-        /// @copydoc fBase_c::fBase_c
         dBase_c();
-        
-        virtual int preCreate(); ///< @copydoc fBase_c::preCreate
-        virtual void postCreate(fBase_c::MAIN_STATE_e status); ///< @copydoc fBase_c::postCreate
 
-        virtual int preDelete(); ///< @copydoc fBase_c::preDelete
-        virtual void postDelete(fBase_c::MAIN_STATE_e status); ///< @copydoc fBase_c::postDelete
+        virtual int preCreate();
+        virtual void postCreate(fBase_c::MAIN_STATE_e status);
 
-        virtual int preExecute(); ///< @copydoc fBase_c::preExecute
-        virtual void postExecute(fBase_c::MAIN_STATE_e status); ///< @copydoc fBase_c::postExecute
+        virtual int preDelete();
+        virtual void postDelete(fBase_c::MAIN_STATE_e status);
 
-        virtual int preDraw(); ///< @copydoc fBase_c::preDraw
-        virtual void postDraw(fBase_c::MAIN_STATE_e status); ///< @copydoc fBase_c::postDraw
+        virtual int preExecute();
+        virtual void postExecute(fBase_c::MAIN_STATE_e status);
 
-        virtual ~dBase_c(); ///< @copydoc fBase_c::~fBase_c
+        virtual int preDraw();
+        virtual void postDraw(fBase_c::MAIN_STATE_e status);
+
+        virtual ~dBase_c();
         
         /// @brief Gets a string describing the kind of this base.
         virtual const char *getKindString() const;
 
-        /// @copydoc fManager_c::searchBaseByProfName
         static dBase_c *searchBaseByProfName(ProfileName profile, const dBase_c* parent);
 
         static void initLoader(); ///< [Unknown purpose. Callbacks do nothing and don't seem to ever be called].
 
-        /// @copydoc fBase_c::createChild
         static dBase_c *createBase(ProfileName profName, dBase_c *parent, unsigned long param, u8 groupType);
-        /// @copydoc fBase_c::createRoot
         static dBase_c *createRoot(ProfileName profName, unsigned long param, u8 groupType);
 
 private:
-        const char* mpKindString; ///< A string describing the kind of this base. [Never read from.]
-        const char* mpNameString; ///< A string representing the name of the profile this base belongs to. [Never read from.]
+        const char* mpKindString; ///< A string describing the kind of this base. [Unused].
+        const char* mpNameString; ///< A string representing the name of the profile this base belongs to. [Unused].
 
-        static int loadAsyncCallback(); ///< @note Unofficial name.
-        static void unloadCallback(); ///< @note Unofficial name.
+        static int loadAsyncCallback(); ///< [Unused, does nothing]. @note Unofficial name.
+        static void unloadCallback(); ///< [Unused, does nothing]. @note Unofficial name.
 };
 
 /// @brief Random seed written to after dBase_c::preDraw.
