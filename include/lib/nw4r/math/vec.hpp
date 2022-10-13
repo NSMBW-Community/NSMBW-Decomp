1998524d6c199337c95584ae1fdd587efcf9cafc
RootCubed <liam@rootcubed.dev>
2022-10-13 18:05:21 +0200

    Make nw4r::math::VEC2 inherit from Vec2

--- include/lib/nw4r/math/vec.hpp
+++ include/lib/nw4r/math/vec.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
+#include <types.h>
+
 namespace nw4r {
 namespace math {
 
 /// @brief A two-dimensional vector.
-struct VEC2 {
-    float x;
-    float y;
-};
+struct VEC2 : Vec2 {};
 
 } // namespace math
 } // namespace nw4r
