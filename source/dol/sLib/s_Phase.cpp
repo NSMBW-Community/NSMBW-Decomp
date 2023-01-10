8c5a407d62ed1bb334b870f0165aa4e06f8e7668
RootCubed <liam@rootcubed.dev>
2023-01-10 22:07:56 +0100

    Decompile sPhase_c

---
+++ source/dol/sLib/s_Phase.cpp
@@ -0,0 +1,27 @@
+#include <dol/sLib/s_Phase.hpp>
+
+sPhase_c::sPhase_c(phaseMethod **methodList, int count) {
+    mpMethodList = methodList;
+    mPhaseLength = count;
+    mCurrMethod = 0;
+}
+
+sPhase_c::CHAIN_RESULT_e sPhase_c::callMethod(void *thisPtr) {
+    if (mCurrMethod >= mPhaseLength) {
+        return DONE;
+    }
+
+    CHAIN_RESULT_e result = OK;
+    while (result == OK) {
+        result = (mpMethodList[mCurrMethod])(thisPtr);
+
+        if (result == OK) {
+            // Go to the next method
+            mCurrMethod++;
+            if (mCurrMethod >= mPhaseLength) {
+                return DONE;
+            }
+        }
+    }
+    return result;
+}
