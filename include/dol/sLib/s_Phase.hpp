3fc4adb0c2980b411614de33c310526d67c76b93
CLF78 <29682335+CLF78@users.noreply.github.com>
2023-01-12 18:50:05 +0100

    Apply suggestions from code review, part 2
    - Add a few extra newlines for code readability
    - Add missing ending newline to dScRestartCrsin header
    - Rename CHAIN_RESULT_e to METHOD_RESULT_e

--- include/dol/sLib/s_Phase.hpp
+++ include/dol/sLib/s_Phase.hpp
@@ -5,12 +5,12 @@ class sPhase_c {
 public:
     /// @brief Return value of a phase method and ::callMethod.
     /// @note Unofficial name.
-    enum CHAIN_RESULT_e {
+    enum METHOD_RESULT_e {
         WAIT, ///< Do not proceed to the next method in the phase.
         OK, ///< Proceed to the next method in the phase.
         DONE ///< The phase is done.
     };
-    typedef CHAIN_RESULT_e (phaseMethod)(void *);
+    typedef METHOD_RESULT_e (phaseMethod)(void *);
 
     /**
      * @brief Constructs a new phase with a given method list.
@@ -20,12 +20,12 @@ public:
      */
     sPhase_c(phaseMethod **methodList, int count);
     /**
-     * @brief Executes the phase until the end is reached or a method returns CHAIN_RESULT_e::WAIT.
+     * @brief Executes the phase until the end is reached or a method returns METHOD_RESULT_e::WAIT.
      * 
      * @param thisPtr A pointer to the owner.
-     * @return CHAIN_RESULT_e::WAIT if the phase is not done yet, and CHAIN_RESULT_e::DONE if the phase is finished.
+     * @return METHOD_RESULT_e::WAIT if the phase is not done yet, and METHOD_RESULT_e::DONE if the phase is finished.
      */
-    CHAIN_RESULT_e callMethod(void *thisPtr);
+    METHOD_RESULT_e callMethod(void *thisPtr);
 
     phaseMethod **mpMethodList; ///< The method list.
     unsigned short mPhaseLength; ///< The length of the method list.
