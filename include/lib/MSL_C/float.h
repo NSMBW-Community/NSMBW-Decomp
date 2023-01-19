1e360efd86959ed8a43a8bcb81d4501d4b4169bc
CLF78 <29682335+CLF78@users.noreply.github.com>
2023-01-19 17:43:47 +0100

    Apply suggestions from code review

--- include/lib/MSL_C/float.h
+++ include/lib/MSL_C/float.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-/// @brief Difference between 1 and the least value greater than 1 that is representable.
+/// @brief Difference between 1 and the smallest 32-bit floating-point value greater than 1.
 #define FLT_EPSILON (1.0f / 0x800000)
 
 #ifdef __cplusplus
