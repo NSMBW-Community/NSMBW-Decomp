353eefd099f51c8644930283e438da3b530a8660
user <user@example.com>
2025-04-28 16:52:36 +0200

    Add header for nw4r::lyt::Material + ::GXColorS10 Kinda hacky, but it works - GXColorS10 should probably be somewhere else, but I'm not sure where...

---
+++ include/lib/nw4r/lyt/lyt_material.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <lib/nw4r/ut/detail/link_list_impl.hpp>
+#include <lib/nw4r/ut/color.hpp>
+
+namespace nw4r {
+namespace lyt {
+
+class GXColorS10 {
+public:
+    s16 r;
+    s16 g;
+    s16 b;
+    s16 a;
+
+    GXColorS10(s16 r, s16 g, s16 b, s16 a) : r(r), g(g), b(b), a(a) { }
+};
+
+class Material {
+public:
+    virtual ~Material();
+
+    ut::detail::LinkListImpl mAnimList;
+    GXColorS10 mTevCols[3];
+    nw4r::ut::Color mTevKCols[4];
+    u32 mGXMemCap; // actually a bitfield
+    u32 mGXMemNum; // actually a bitfield
+    void * mpGXMem;
+    char mName[21];
+    bool mbUserAllocated;
+
+    char pad[2];
+};
+
+} // namespace lyt
+} // namespace nw4r
