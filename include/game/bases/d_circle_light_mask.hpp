9a286bbc9fde6ad95c7a4ee5ca65abe8e41d43eb
RootCubed <liam@rootcubed.dev>
2025-05-10 10:48:05 +0200

    daIceBall_c almost matching

--- include/game/bases/d_circle_light_mask.hpp
+++ include/game/bases/d_circle_light_mask.hpp
@@ -1,7 +1,14 @@
 #pragma once
 #include <game/mLib/m_vec.hpp>
 
-class dCircleLightMask_c {
+class dLightMask_c {
+public:
+    virtual u32 init(void*, u32) = 0; ///< @unofficial
+    virtual u32 execute() = 0;
+    virtual u32 draw() = 0;
+};
+
+class dCircleLightMask_c : public dLightMask_c {
 
 public:
     dCircleLightMask_c() { reset(); };
