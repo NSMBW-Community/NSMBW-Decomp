a955f93d36c53b18b6dfa2871041796bf4240204
RootCubed <liam@rootcubed.dev>
2025-05-29 13:34:21 +0200

    dBg_c 93% matching

--- include/game/bases/d_s_stage.hpp
+++ include/game/bases/d_s_stage.hpp
@@ -35,6 +35,7 @@ public:
 
     typedef void (*changePosFunc)(mVec3_c *);
     static void setChangePosFunc(int);
+    static dScStage_c *getInstance() { return m_instance; }
 
     static void setTitleReplayRandomTable();
 
