4a6c1a8bd45e7b645add02cfd218ba8b7d9b1848
RootCubed <liam@rootcubed.dev>
2025-08-10 23:23:41 +0200

    `LytBase_c::FUN_800c9770` 99.10% matching

--- include/game/mLib/m_video.hpp
+++ include/game/mLib/m_video.hpp
@@ -5,6 +5,9 @@ class mVideo {
 public:
     static void create();
 
+    float getScaledWidth(float scale) const { return mRenderModeObj.fbWidth / scale; }
+    float getScaledHeight(float scale) const { return mRenderModeObj.efbHeight / scale; }
+
     static float getSmth(float offs) { return (m_video->mRenderModeObj.efbHeight - offs) * 0.5f; }
 
     GXRenderModeObj &mRenderModeObj;
