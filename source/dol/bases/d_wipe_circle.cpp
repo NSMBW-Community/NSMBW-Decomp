b76405578deae17c24c2981c6596a9ff18d15faf
CLF78 <29682335+CLF78@users.noreply.github.com>
2025-08-23 13:04:11 +0200

    Add documentation and code cleanup

--- source/dol/bases/d_wipe_circle.cpp
+++ source/dol/bases/d_wipe_circle.cpp
@@ -7,8 +7,8 @@
 
 dWipeCircle_c *dWipeCircle_c::m_instance;
 
-dWipeCircle_c::dWipeCircle_c(nw4r::ut::Color color, mFaderBase_c::EStatus stat) :
-mFaderBase_c(mColor(color.rgba), stat) {
+dWipeCircle_c::dWipeCircle_c(nw4r::ut::Color color, mFaderBase_c::EStatus status) :
+mFaderBase_c(mColor(color.rgba), status) {
     m_instance = this;
     mIsCreated = false;
 }
@@ -18,18 +18,21 @@ dWipeCircle_c::~dWipeCircle_c() {
 }
 
 bool dWipeCircle_c::createLayout() {
-    static const char *AnmNameTbl[] = {
+    static const char *AnmNameTbl[ANIM_NAME_COUNT] = {
         "wipeCircle_00_inWindow.brlan",
         "wipeCircle_00_outWindow.brlan"
     };
 
-    static const char *GROUP_NAME_DT[] = {
+    static const char *GROUP_NAME_DT[ANIM_COUNT] = {
         "A00_inWindow",
         "B00_outWindow"
     };
-    static const int ANIME_INDEX_TBL[] = {0, 1};
+    static const int ANIME_INDEX_TBL[ANIM_COUNT] = {
+        inWindow,
+        outWindow
+    };
 
-    static const char *WPANE_NAME_DT[] = {
+    static const char *WPANE_NAME_DT[W_COUNT] = {
         "W_circle_00"
     };
 
@@ -37,28 +40,27 @@ bool dWipeCircle_c::createLayout() {
         return true;
     }
 
-    bool res = mLyt.ReadResource("wipeCircle/wipeCircle.arc", false);
-    if (!res) {
+    if (!mLyt.ReadResource("wipeCircle/wipeCircle.arc", false)) {
         return false;
     }
 
-    mLyt.build("wipeCircle_00.brlyt", 0);
-    mLyt.AnimeResRegister(AnmNameTbl, 2);
-    mLyt.GroupRegister(GROUP_NAME_DT, ANIME_INDEX_TBL, 2);
+    mLyt.build("wipeCircle_00.brlyt", nullptr);
+    mLyt.AnimeResRegister(AnmNameTbl, ANIM_NAME_COUNT);
+    mLyt.GroupRegister(GROUP_NAME_DT, ANIME_INDEX_TBL, ANIM_COUNT);
     mpRootPane = mLyt.getRootPane();
-    mLyt.WPaneRegister(WPANE_NAME_DT, mpWnd, 1);
+    mLyt.WPaneRegister(WPANE_NAME_DT, mpWnd, W_COUNT);
 
-    mCenterPos.x = 0.0;
-    mCenterPos.y = 0.0;
+    mCenterPos.x = 0.0f;
+    mCenterPos.y = 0.0f;
     mIsCreated = true;
-    mField_1c1 = false;
-    mField_1c2 = false;
+    mHasTarget = false;
+    mUseCenterPos = false;
 
     mLyt.AllAnimeEndSetup();
 
     mpRootPane->setVisible(false);
     mLyt.mDrawOrder = 154;
-    mAction = 3;
+    mAction = IDLE;
 
     return true;
 }
@@ -66,15 +68,17 @@ bool dWipeCircle_c::createLayout() {
 typedef void (dWipeCircle_c::*actMeth)();
 
 int dWipeCircle_c::calc() {
-    static const actMeth Proc_tbl[] = {
+    static const actMeth Proc_tbl[ACTION_COUNT] = {
         dWipeCircle_c::OpenSetup,
         dWipeCircle_c::AnimeEndCheck,
         dWipeCircle_c::CloseSetup
     };
+
     if (!mIsCreated) {
         return 1;
     }
-    if (mAction != 3) {
+
+    if (mAction != IDLE) {
         (this->*Proc_tbl[mAction])();
         mLyt.AnimePlay();
         mLyt.calc();
@@ -93,29 +97,32 @@ void dWipeCircle_c::draw() {
 #pragma ppc_iro_level 2
 
 void dWipeCircle_c::CenterPosSet() {
-    mVec2_c newPos(0.0f, 0.0f);
+    mVec2_c centerPos(0.0f, 0.0f);
     mpRootPane->mPos = mVec3_c(0.0f, 0.0f, 0.0f);
-    if (!mField_1c1) {
+    if (!mHasTarget) {
         return;
     }
-    mField_1c1 = false;
-    if (mField_1c2) {
-        mField_1c2 = false;
-        newPos.x = mCenterPos.x;
-        newPos.y = mCenterPos.y;
+
+    mHasTarget = false;
+    if (mUseCenterPos) {
+        mUseCenterPos = false;
+        centerPos.x = mCenterPos.x;
+        centerPos.y = mCenterPos.y;
     } else {
-        int plNo = daPyDemoMng_c::mspInstance->mPlNo;
+        int targetPlayerNo = daPyDemoMng_c::mspInstance->mPlNo;
         if (dScStage_c::m_KoopaJrEscape) {
-            plNo = 0;
+            targetPlayerNo = 0;
         }
-        if (plNo < 0) {
+        if (targetPlayerNo < 0) {
             return;
         }
-        dBaseActor_c *player = daPyMng_c::getPlayer(plNo);
-        if (player == nullptr) {
+
+        dAcPy_c *targetPlayer = daPyMng_c::getPlayer(targetPlayerNo);
+        if (targetPlayer == nullptr) {
             return;
         }
-        static float PLAYER_OFFSET_Y_DATA_TBL[4][7] = {
+
+        static float PLAYER_OFFSET_Y_DATA_TBL[PLAYER_COUNT][POWERUP_COUNT] = {
             {
                 dfukidashiInfo_c::c_MARIO_NORMAL_OFFSET_Y,
                 dfukidashiInfo_c::c_MARIO_SUPER_OFFSET_Y,
@@ -153,27 +160,28 @@ void dWipeCircle_c::CenterPosSet() {
                 dfukidashiInfo_c::c_KINOPIO_ICE_OFFSET_Y
             }
         };
-        int plType = daPyMng_c::getPlayerType(plNo);
-        int plMode = daPyMng_c::getPlayerMode(plType);
+        int targetPlayerType = daPyMng_c::getPlayerType(targetPlayerNo);
+        int targetPlayerMode = daPyMng_c::getPlayerMode(targetPlayerType);
 
-        mVec3_c pos;
-        pos.x = player->mPos.x;
-        float offs = PLAYER_OFFSET_Y_DATA_TBL[plType][plMode];
-        pos.y = player->mPos.y + offs * 0.5f;
-        pos.z = player->mPos.z;
-        dGameCom::getGlbPosToLyt(pos);
-        newPos.x = pos.x;
-        newPos.y = pos.y;
+        mVec3_c targetPos;
+        targetPos.x = targetPlayer->mPos.x;
+        float offs = PLAYER_OFFSET_Y_DATA_TBL[targetPlayerType][targetPlayerMode];
+        targetPos.y = targetPlayer->mPos.y + offs * 0.5f;
+        targetPos.z = targetPlayer->mPos.z;
+        dGameCom::getGlbPosToLyt(targetPos);
+        centerPos.x = targetPos.x;
+        centerPos.y = targetPos.y;
     }
-    mpRootPane->mPos = mVec3_c(newPos, 0.0f);
+
+    mpRootPane->mPos = mVec3_c(centerPos, 0.0f);
 }
 
 #pragma pop
 
 void dWipeCircle_c::OpenSetup() {
     mLyt.AllAnimeEndSetup();
-    mLyt.AnimeStartSetup(1, false);
-    mAction = 1;
+    mLyt.AnimeStartSetup(outWindow, false);
+    mAction = ANIME_END_CHECK;
     CenterPosSet();
 }
 
@@ -181,37 +189,37 @@ void dWipeCircle_c::AnimeEndCheck() {
     if (!mLyt.isAnime(-1)) {
         if (getStatus() == FADE_IN) {
             mStatus = HIDDEN;
-            mFlag |= 1;
-            mpRootPane->mFlags &= ~1;
+            mFlag |= FADE_IN_COMPLETE;
+            mpRootPane->setVisible(false);
         } else {
             mStatus = OPAQUE;
-            mFlag |= 2;
+            mFlag |= FADE_OUT_COMPLETE;
         }
-        mCenterPos.x = 0;
-        mCenterPos.y = 0;
-        mAction = 3;
+        mCenterPos.x = 0.0f;
+        mCenterPos.y = 0.0f;
+        mAction = IDLE;
     }
 }
 
 void dWipeCircle_c::CloseSetup() {
     mLyt.AllAnimeEndSetup();
-    mLyt.AnimeStartSetup(0, false);
-    mAction = 1;
+    mLyt.AnimeStartSetup(inWindow, false);
+    mAction = ANIME_END_CHECK;
     CenterPosSet();
 }
 
-void dWipeCircle_c::setStatus(mFaderBase_c::EStatus stat) {
-    if (stat == OPAQUE) {
+void dWipeCircle_c::setStatus(mFaderBase_c::EStatus status) {
+    if (status == OPAQUE) {
         mStatus = OPAQUE;
-        mpWnd[0]->mAlpha = -1;
-        mLyt.ReverseAnimeStartSetup(1, false);
+        mpWnd[W_circle_00]->mAlpha = 255;
+        mLyt.ReverseAnimeStartSetup(outWindow, false);
         mpRootPane->setVisible(true);
         mLyt.AnimePlay();
         mLyt.calc();
-    } else if (stat == HIDDEN) {
+    } else if (status == HIDDEN) {
         mStatus = HIDDEN;
-        mpWnd[0]->mAlpha = 0;
-        mLyt.ReverseAnimeStartSetup(0, false);
+        mpWnd[W_circle_00]->mAlpha = 0;
+        mLyt.ReverseAnimeStartSetup(inWindow, false);
         mpRootPane->setVisible(true);
         mLyt.AnimePlay();
         mLyt.calc();
@@ -221,7 +229,7 @@ void dWipeCircle_c::setStatus(mFaderBase_c::EStatus stat) {
 bool dWipeCircle_c::fadeIn() {
     bool res = mFaderBase_c::fadeIn();
     if (res) {
-        mAction = 0;
+        mAction = OPEN_SETUP;
     }
     return res;
 }
@@ -229,7 +237,7 @@ bool dWipeCircle_c::fadeIn() {
 bool dWipeCircle_c::fadeOut() {
     bool res = mFaderBase_c::fadeOut();
     if (res) {
-        mAction = 2;
+        mAction = CLOSE_SETUP;
     }
     return res;
 }
