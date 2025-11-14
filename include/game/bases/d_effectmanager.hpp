9f198c4b5020ae2f007c057e7d20659b0374d194
RootCubed <liam@rootcubed.dev>
2025-11-14 15:06:24 +0100

    Clean up `d_enemy_death.cpp`

--- include/game/bases/d_effectmanager.hpp
+++ include/game/bases/d_effectmanager.hpp
@@ -6,7 +6,7 @@ public:
     static void SetIceBallMissshitEffect(mVec3_c *);
     static void calcGroupForWm();
     static void calcGroup();
-    static void FUN_800943d0(mVec3_c *); ///< @unofficial
+    static void SetVsHitEffect(mVec3_c *); ///< @unofficial
 
     static bool m_firstCalc;
 };
