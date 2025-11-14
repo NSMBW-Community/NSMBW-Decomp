77d375d7c7ab0113751c9f7aefd6a539d2d03a0d
RootCubed <liam@rootcubed.dev>
2025-11-14 22:39:13 +0100

    Implement dAudio header

--- include/game/snd/snd_audio_mgr.hpp
+++ include/game/snd/snd_audio_mgr.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <types.h>
+#include <lib/nw4r/snd.h>
 
 class SndAudioMgr {
 public:
     void startSystemSe(unsigned int soundID, unsigned long);
+    u32 get3DCtrlFlag(unsigned long);
+    void setSoundPosition(nw4r::snd::SoundHandle *p, const nw4r::math::VEC2 &pos);
+
+    u8 mPad1[0x100];
+    nw4r::snd::SoundArchive *mpSndArc;
+    u8 mPad2[0x4b8];
+    nw4r::snd::SoundArchivePlayer &mArcPlayer;
 
 public:
     static SndAudioMgr *sInstance;
