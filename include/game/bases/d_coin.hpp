dfff01f0cb7a64850c10b0c22d3ea4388050a073
TomCube <121310847+yomcube@users.noreply.github.com>
2025-11-30 20:42:10 -0600

    Match d_coin.cpp

--- include/game/bases/d_coin.hpp
+++ include/game/bases/d_coin.hpp
@@ -1,12 +1,13 @@
 #pragma once
+#include <game/mLib/m_angle.hpp>
 #include <types.h>
 
 class dCoin_c {
 public:
     static int execute();
-protected:
-    static s16 m_shapeAngle[3];
-    static s16 m_h_shapeAngle[3];
+private:
+    static mAng3_c m_shapeAngle;
+    static mAng3_c m_h_shapeAngle;
     static u8 m_frame;
     static u8 m_ptn_no;
     static u8 m_h_ptn_no;
