eda6323f6148bee1cee4ef2d1671413dab2747ca
CLF78 <29682335+CLF78@users.noreply.github.com>
2025-12-23 11:36:11 +0100

    Add missed documentation for #88

--- include/game/bases/d_s_autoselect.hpp
+++ include/game/bases/d_s_autoselect.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <game/bases/d_scene.hpp>
 
+/**
+ * @brief An empty scene that does nothing.
+ * @details Presumably, it was used to automatically cycle between stages, which was reimplemented
+ * in the Shield version as the @p AUTO_PILOTING .INI configuration.
+ * @ingroup bases
+ * @unused
+ */
 class dScAutoSelect_c : public dScene_c {
 public:
-    dScAutoSelect_c() {}
-    virtual ~dScAutoSelect_c() {}
+    dScAutoSelect_c() {} ///< @copydoc dScene_c::dScene_c
+    virtual ~dScAutoSelect_c() {} ///< @copydoc dScene_c::~dScene_c
 };
