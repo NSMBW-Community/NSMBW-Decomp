9fac8a13d72b2e8ae0c030047789a4de343f054b
RootCubed <liam@rootcubed.dev>
2026-04-06 11:33:24 +0200

    some initial cleanup

--- include/game/bases/d_a_en_snake_block.hpp
+++ include/game/bases/d_a_en_snake_block.hpp
@@ -1,38 +1,9 @@
 #pragma once
 #include <game/bases/d_enemy.hpp>
+#include <game/bases/d_bg_ctr.hpp>
 #include <game/bases/d_heap_allocator.hpp>
 #include <game/mLib/m_3d/anm_mat_clr.hpp>
 #include <game/mLib/m_3d/mdl.hpp>
-
-class dBg_ctr_c {
-public:
-    dActor_c *mpActor;
-    u8 mPad1[0x9c];
-    mVec2_c m_a0;
-    mVec2_c m_ac;
-    u8 mPad2[0xc];
-    short *m_bc;
-    u8 mPad4[0x8];
-    int m_c8;
-    int m_cc;
-    u32 mFlags;
-    int m_d4;
-    u8 mpPad5[0x4];
-    bool m_dc;
-    u8 mpPad6[0x3];
-    bool m_e0;
-    u8 m_e1;
-    u8 m_e2;
-
-    dBg_ctr_c();
-    ~dBg_ctr_c();
-
-    void release();
-    void entry();
-    void set(dActor_c *, float, float, float, float, void *, void *, void *, int, int, void *);
-    void calc();
-    void setOfs(float, float, float, float, void *);
-};
 
 class daEnSnakeBlock_c : public dEn_c {
 public:
@@ -42,13 +13,13 @@ public:
         dBlock_c() : mpOwner(nullptr), m_1b4(0) {}
         virtual ~dBlock_c() { mBgCtr.release(); }
 
-        void createMdl(dHeapAllocator_c *alloc);
+        void createMdl(mAllocator_c *allocator);
         void doDelete();
 
         void calcAnm();
         void setAnmClr(const char *name);
 
-        void draw(const mVec3_c &pos);
+        void draw(const mVec3_c &offset);
 
         void initBgCtr(daEnSnakeBlock_c *parent, mVec3_c &blockPos, int unused);
         void calcBgCtr();
@@ -58,19 +29,18 @@ public:
 
         mVec3_c getPos() const { return mPos; }
         void setPos(mVec3_c pos) { mPos = pos; }
-        void setSpeed(float x, float y, float z) { mSpeed.set(x, y, z); }
-        void resetSpeed() { mSpeed.set(0.0f, 0.0f, 0.0f); }
-        void process() {
+        void releaseBgCtr() {
             if (mBgCtr.m_dc) {
                 mBgCtr.release();
             }
-            setFallCollapse();
+        }
+        void updatePos() {
             mPos += mSpeed;
         }
 
-        static void callBackF(dActor_c *self, dActor_c *other);
-        static void callBackH(dActor_c *self, dActor_c *other);
-        static void callBackW(dActor_c *self, dActor_c *other, u8);
+        static dBg_ctr_c::CallbackF callBackF;
+        static dBg_ctr_c::CallbackH callBackH;
+        static dBg_ctr_c::CallbackW callBackW;
 
         nw4r::g3d::ResFile mResFile;
         m3d::mdl_c mModel;
@@ -136,8 +106,8 @@ public:
     bool chkCollapseDelete(); ///< Returns if the Tail is below the screen edge (meaning the collapse has ended, and the actor can be deleted)
     bool chkOffScreen(); ///< Returns if the Snake Block is far past the right edge of the screen (used to delete it)
 
-    dCtrlBlock_c *getHeadBlock() { return &mCtrlBlock[0]; }
-    dCtrlBlock_c *getTailBlock() { return &mCtrlBlock[1]; }
+    dCtrlBlock_c &getHeadBlock() { return mCtrlBlock[0]; }
+    dCtrlBlock_c &getTailBlock() { return mCtrlBlock[1]; }
 
     dHeapAllocator_c mAllocator;
 
@@ -156,15 +126,18 @@ public:
     int mCreateAnmBlockNum;
     u8 _pad[4];
 
+    ACTOR_PARAM_CONFIG(Param_00, 0, 4);
     ACTOR_PARAM_CONFIG(RailIdx, 4, 4);
+    ACTOR_PARAM_CONFIG(Param_08, 8, 2);
+    ACTOR_PARAM_CONFIG(Param_0c, 12, 8);
     ACTOR_PARAM_CONFIG(RailStartIdx, 12, 8);
+    ACTOR_PARAM_CONFIG(Param_14, 20, 1);
+    ACTOR_PARAM_CONFIG(StopState, 28, 4);
 
     static const float sc_FallAccel;
     static const float sc_FallMaxSpeed;
     static const float sc_snakeSpeeds[3];
     static const float sc_snakeSpeeds2[3];
-    static const float sc_snakeDir[2];
-    static sStateID_c *sc_stopStates[4];
     static mVec2_c sc_ctrlPosMods[5];
     static int sc_glbSnakeNum;
     static const int scSnakeSoundID;
