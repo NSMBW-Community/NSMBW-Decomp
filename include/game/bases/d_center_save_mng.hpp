e5ca3f5d55ae3d80d027afb60fbfd0378b1fea11
RootCubed <liam@rootcubed.dev>
2026-09-13 13:51:45 +0200

    Match and link `dCenterSaveMng_c`

---
+++ include/game/bases/d_center_save_mng.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <game/cLib/c_line.hpp>
+#include <game/mLib/m_vec.hpp>
+
+class dCenterSaveMng_c {
+public:
+    dCenterSaveMng_c();
+    ~dCenterSaveMng_c();
+
+    bool center_check(const mVec3_c* pos, u8 id, short* outValue);
+
+    void center_entry(const mVec3_c* pos, u8 id, short value);
+
+    static void allDoDelete();
+
+private:
+    /// @unofficial
+    class Link_c : public cLineNd_c {
+    public:
+        Link_c() {
+            mpSelf = this;
+            ms_linkManager.addLastLineNode(this);
+        }
+
+        ~Link_c() {
+            ms_linkManager.removeLineNode(this);
+        }
+
+        float getX() const { return mPos.x; }
+        float getY() const { return mPos.y; }
+
+        Link_c *mpSelf;
+        mVec3_c mPos;
+        short mValue;
+        u8 mID;
+    };
+
+    static dCenterSaveMng_c *m_instance;
+    static cLineMg_c ms_linkManager;
+};
---
+++ source/dol/bases/d_center_save_mng.cpp
@@ -0,0 +1,74 @@
+#include <game/bases/d_center_save_mng.hpp>
+
+dCenterSaveMng_c *dCenterSaveMng_c::m_instance;
+cLineMg_c dCenterSaveMng_c::ms_linkManager;
+
+dCenterSaveMng_c::dCenterSaveMng_c() {
+    m_instance = this;
+}
+
+dCenterSaveMng_c::~dCenterSaveMng_c() {
+    allDoDelete();
+    m_instance = nullptr;
+}
+
+void dCenterSaveMng_c::allDoDelete() {
+    Link_c *link;
+
+    Link_c *curr = (Link_c *) ms_linkManager.getFirst();
+
+    while (curr != nullptr) {
+        link = curr->mpSelf;
+        curr = (Link_c *) curr->getNext();
+
+        delete link;
+    }
+}
+
+bool dCenterSaveMng_c::center_check(const mVec3_c *pos, u8 id, short *outValue) {
+    Link_c *link;
+
+    Link_c *curr = (Link_c *) ms_linkManager.getFirst();
+    bool found = false;
+    *outValue = 0;
+
+    while (curr != nullptr) {
+        link = curr->mpSelf;
+        curr = (Link_c *) curr->getNext();
+
+        if (pos->x == link->getX() && pos->y == link->getY() && id == link->mID) {
+            *outValue = link->mValue;
+            found = true;
+            break;
+        }
+    }
+
+    return found;
+}
+
+void dCenterSaveMng_c::center_entry(const mVec3_c *pos, u8 id, short value) {
+    Link_c *link;
+
+    Link_c *curr = (Link_c *) ms_linkManager.getFirst();
+    bool found = false;
+
+    while (curr != nullptr) {
+        link = curr->mpSelf;
+        curr = (Link_c *) curr->getNext();
+
+        if (pos->x == link->getX() && pos->y == link->getY() && id == link->mID) {
+            link->mValue = value;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        Link_c *newEntry = new Link_c();
+        if (newEntry != nullptr) {
+            newEntry->mPos = *pos;
+            newEntry->mID = id;
+            newEntry->mValue = value;
+        }
+    }
+}
